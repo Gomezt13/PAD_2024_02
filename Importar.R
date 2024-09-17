c220caf96f0a7e59c9652c3159f20bf29694f795
Gomezt13 <eulergomez6@.com>
2024-09-17 15:40:27 -0500

    impot

--- Importar.R
+++ Importar.R
@@ -18,3 +18,14 @@ install.packages("openxlsx")
 library(openxlsx)
 
 xlsxdt <- read.xlsx(xlsxFile = "DATOS_2024_02/LA MOLINA 2014 POTATO WUE (FB).xlsx", sheet = "fb")
+
+
+# importar de google sheet ------------------------------------------------
+
+library(inti)
+library(googlesheets4)
+
+url <- "https://docs.google.com/spreadsheets/d/15r7ZwcZZHbEgltlF6gSFvCTFA-CFzVBWwg3mFlRyKPs/edit?gid=172957346#gid=172957346"
+gs <- as_sheets_id(url)
+
+fb <- googlesheets4::range_read(ss = gs, sheet = "fb")
