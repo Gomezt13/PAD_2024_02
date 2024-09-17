080d62177752870b53bd9d1167d88365220707cb
Gomezt13 <eulergomez6@.com>
2024-09-17 15:11:13 -0500

    importar dt

--- Importar.R
+++ Importar.R
@@ -5,7 +5,13 @@
 
 csvdt <- read.csv(file = "DATOS_2024_02/LA MOLINA 2014 POTATO WUE (FB) - fb.csv")
 
+
+# importar tsv ------------------------------------------------------------
+
 tsv <- read.delim(file = "DATOS_2024_02/LA MOLINA 2014 POTATO WUE (FB) - fb.tsv", sep = "\t", header = TRUE)
+
+
+# importar xlsx -----------------------------------------------------------
 
 install.packages("openxlsx")
 
