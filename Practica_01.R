bd7b6a7834447e3ec6808d1547761d660aed078e
Gomezt13 <eulergomez6@.com>
2024-09-22 20:46:00 -0500

    tidyverse

--- Practica_01.R
+++ Practica_01.R
@@ -9,7 +9,7 @@ paste(saludo,name)
 
 # peogramción antigua
 
-notas <- c(12,65,98,36,95,65)
+notas <- c(15,36,25,85,95,74)
 
 notas_suma <- notas+1
 
@@ -17,7 +17,9 @@ notas_prom <- mean(notas_suma)
 
 # programación funcional
 
-notas_promedio <- c(15,36,25,85,95,74) %>% +1 %>% mean()
+notas_promedio <- c(15,36,25,85,95,74) %>% 
+  + 1 %>% 
+  mean()
 
 
 nota <- c(15,36,25,85,95,74) |>  mean()
