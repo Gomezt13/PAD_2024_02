6f81c8f858b48431c9c19db573354a7118ebd31f
Gomezt13 <eulergomez6@.com>
2024-09-30 10:03:34 -0500

    DBCA

--- Diseños_experimentales.R
+++ Diseños_experimentales.R
@@ -4,6 +4,11 @@ install.packages("agricolae")
 # Cargar el paquete agricolae
 library(agricolae)
 
+
+# DCA ---------------------------------------------------------------------
+
+
+
 # Definir los niveles de dosis de fertilización nitrogenada
 dosis <- c("0", "50", "100")
 
@@ -18,3 +23,34 @@ print(diseño)
 
 # Visualizar el diseño
 diseño$book
+
+
+# DBCA --------------------------------------------------------------------
+
+library(agricolae)
+library(tidyverse)
+
+# fertilizante: 0, 50, 100
+# cultivar: canchan y peruanita
+trt<-c(3, 2) # factorial 3x2
+
+
+outdesign <-design.ab(trt, r=4, design = "rcbd")
+book<-outdesign$book
+
+book %>% str()
+
+ds <- book %>% 
+  mutate(ferti = case_when(
+    A %in% 1 ~ "0"
+    ,  A %in% 2 ~ "50"
+    ,  A %in% 3 ~ "100"
+  )) %>% 
+  mutate(cultivar = case_when(
+    B %in% 1 ~ "canchan"
+    ,  B %in% 2 ~ "peruanita"
+  )) 
+
+
+ds %>% 
+  openxlsx::write.xlsx(file = "dbca.xlsx")
